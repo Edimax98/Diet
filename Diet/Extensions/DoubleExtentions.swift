a1afe26917c8cc1b7ae2872318e398f06a2ddcec
Edimax98 <>
2018-11-30 18:25:00 +0300

    Ads added; FB Core intergrated; Fatnnes index calculating implemented; Results of test are showing correcltly.

---
+++ Diet/Extensions/DoubleExtentions.swift
@@ -0,0 +1,9 @@
+//
+//  DoubleExtentions.swift
+//  Diet
+//
+//  Created by Даниил on 28/11/2018.
+//  Copyright © 2018 Даниил. All rights reserved.
+//
+
+import Foundation
---
+++ Diet/Models/TestResult.swift
@@ -0,0 +1,9 @@
+//
+//  TestResult.swift
+//  Diet
+//
+//  Created by Даниил on 28/11/2018.
+//  Copyright © 2018 Даниил. All rights reserved.
+//
+
+import Foundation
