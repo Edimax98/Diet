6eadd8d5d36bacc6cf7081f84573f14569f0a613
Edimax98 <>
2018-12-03 19:41:58 +0300

    Animations added; Fatness index VC added;

---
+++ Diet/FattnessIndex/FatnessCategoryCell.swift
@@ -0,0 +1,18 @@
+//
+//  FatnessCategoryCell.swift
+//  Diet
+//
+//  Created by Даниил on 03/12/2018.
+//  Copyright © 2018 Даниил. All rights reserved.
+//
+
+import UIKit
+
+class FatnessCategoryCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Diet/FattnessIndex/FatnessIndexViewContoller.swift
@@ -0,0 +1,18 @@
+//
+//  FatnessIndexViewContoller.swift
+//  Diet
+//
+//  Created by Даниил on 03/12/2018.
+//  Copyright © 2018 Даниил. All rights reserved.
+//
+
+import UIKit
+
+class FatnessIndexViewContoller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
